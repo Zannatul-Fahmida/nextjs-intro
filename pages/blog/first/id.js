6d661e9b5f4d979ccfd2d594dd81597419793363
zannatul <jannatulfahmida15@gmail.com>
2023-01-07 19:58:08 +0600

    first commit

---
+++ pages/blog/first/id.js
@@ -0,0 +1,5 @@
+function Id() {
+    return <h1>Id Page</h1>;
+  }
+  
+  export default Id;
---
+++ pages/product/sweater.js
@@ -0,0 +1,5 @@
+function Sweater() {
+    return <h1>Details about product sweater 2</h1>
+  }
+  
+  export default Sweater;
